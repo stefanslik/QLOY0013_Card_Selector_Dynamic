b25877c3903386b75e2c406a6f901f774cf1a681
stefanslik <stefan@slik.com.au>
2016-06-29 10:08:13 +1000

    latest
    latest

--- 320x100/js/banner.js
+++ 320x100/js/banner.js
@@ -2,7 +2,7 @@ var tl;
 var imagesLoaded = false;
 
 $(document).ready(function(){
-  preloadimages(["logos.png", "shape-mask.svg", "shape-red.svg", "shape-blue.svg", "shape-red-end-frame.svg", "shape-red-end-frame2.svg"]).done(function (images) {
+  preloadimages(["shape-mask.svg", "shape-red.svg", "shape-blue.svg","card-shadow.png","black-visa-back.png","black-visa-front.png","shape-red-base.svg","white-shape.svg","logo-ff.svg"]).done(function (images) {
 
       // ONCE IMAGES ARE PRE-LOADED BEGIN ANIMATION
        initCSS();
@@ -35,7 +35,17 @@ function initCSS() {
     TweenLite.set($("#red-shape"), {alpha: .9, rotation: -20, scaleX: 0.3, scaleY: 0.3, x: 130, y: 73, overwrite: "none", force3D: false});
     TweenLite.set($("#mask-shape"), {rotation: 30, scaleX: 0.2, scaleY: 0.2, x: -2235, y: -1275, overwrite: "none", force3D: false});
     TweenLite.set($(".shapes"), {rotation: 10, transformOrigin: "50% 50%", overwrite: "none", force3D: false});
-    TweenLite.set($("#white-shape"), {x: -95});
+    TweenLite.set($("#white-shape"), {x:-90,rotation: -15});
+    TweenLite.set($("#white-shape-2"), {x:80,rotation: -15});
+    TweenLite.set($("#card-wrapper"), {x:80, perspective:800});
+    TweenLite.set($(".card"), {transformStyle:"preserve-3d"});
+    TweenLite.set($(".back"), {rotationY:-180});
+    TweenLite.set($(".logos"), {x:-80});
+    TweenLite.set($(["#impact-copy01", "#f2-copy", "#f3-copy"]), {alpha:0});
+    TweenLite.set($([".back", ".front"]), {backfaceVisibility:"hidden"});
+
+
+
 }
 
 
@@ -80,21 +90,15 @@ function backupAnimation() {
     tl.to(".shapes", 2, {rotation: -160, transformOrigin: "50% 50%"}, "0")
     tl.from("#red-shape", .5, {rotation: 200, transformOrigin: "50% 50%", x: 135, y: 85, ease: Back.easeOut}, "0")
     tl.to("#mask-shape", 1.5, {rotation: 0, scaleX: 1.3, scaleY: 1.3, x: -2100, y: -1315,  ease: Sine.easeInOut}, "0")
-
     tl.to("#red-shape", 1, {rotation: -80, scaleX: 1.5, scaleY: 1.5, x: 231, y: 55, ease: Sine.easeInOut}, ".3")
     tl.to("#blue-shape", 1, {rotation: -140, scaleX: 1, scaleY: 1, x: -4, y: 117, ease: Sine.easeInOut}, ".3")
-
     tl.to("#mask-shape", .7, {rotation: 0, scaleX: 4.2, scaleY: 4.2, x: -1650, y: -1315, ease: Back.easeIn}, "1.1")
     tl.to("#blue-shape", .7, {rotation: -230, scaleX: 1.5, scaleY: 1.5, x: -260, y: 120, ease: Sine.easeOut}, "1.45")
-//    tl.to("#red-shape", .7, {rotation: 0, scaleX: 4, scaleY: 4, opacity: 0, x: 575, y: 75, ease: Sine.easeInOut}, "1.45")
     tl.to("#red-shape", .7, {rotation: 0, scaleX: 4, scaleY: 4, opacity: 0, x: 495, y: 75, ease: Sine.easeInOut}, "1.45")
-
     tl.from("#f1-copy", 1.5, {alpha: 0, ease: Sine.easeOut}, "1.7")
     tl.from(".bg-image", 2, {alpha: 0, ease: Sine.easeOut}, "0")
 
     //FRAME 02
-//    tl.to("#f1-copy", .5, {alpha: 0, ease: Sine.easeIn}, "4.7")
-//    tl.from("#f2-copy", 1.5, {alpha: 0, ease: Sine.easeOut}, "5.2")
 
     tl.to("#blue-shape", .7, {x: -400, y: 80, ease: Sine.easeIn}, "2.5")
     tl.to("#red-shape", .7, {x: 895, y: 75, ease: Sine.easeIn}, "2.5")
@@ -103,17 +107,15 @@ function backupAnimation() {
     tl.from(".logos", 2, {alpha: 0, ease: Sine.easeOut}, "2.5")
     tl.to("#red-curved", 2, {alpha: 0, ease: Sine.easeOut}, "4")
 
-    //tl.to("#f2-copy", .5, {alpha: 0, ease: Sine.easeOut}, "10")
+
 
     //FRAME 03
 
-//    tl.to("#f1-copy", 1, {x: -100}, "5")
+    tl.to("#f1-copy", 1, {x: -100}, "5")
 //    tl.from("#f3-copy", 1, {alpha: 0, y: -10, ease: Sine.easeOut}, "10.5")
     tl.to("#f1-copy", 1, {alpha: 1, y: 0, ease: Sine.easeOut}, "6.5")
     tl.from(".terms-wrapper", 1, {alpha: 0, ease: Sine.easeOut}, "6.5")
     tl.from(".button", 1.7, {alpha: 0, scaleX: .5, scaleY: .5, ease: Expo.easeOut}, "3")
-
-
 
 }
 
@@ -123,9 +125,6 @@ function startAnimation() {
     $(".container").show();
     $(".loader").hide();
 
-
-    // WE USE GREENSOCK ANIMATION PLATFORM, SEE THIS FOR MORE INFO > http://greensock.com/timelinelite
-
     tl = new TimelineLite();
 
     //FRAME 01
@@ -133,45 +132,42 @@ function startAnimation() {
     tl.to(".shapes", 2, {rotation: -160, transformOrigin: "50% 50%"}, "0")
     tl.from("#red-shape", .5, {rotation: 200, transformOrigin: "50% 50%", x: 135, y: 85, ease: Back.easeOut}, "0")
     tl.to("#mask-shape", 1.5, {rotation: 0, scaleX: 1.3, scaleY: 1.3, x: -2100, y: -1315,  ease: Sine.easeInOut}, "0")
-
     tl.to("#red-shape", 1, {rotation: -80, scaleX: 1.5, scaleY: 1.5, x: 231, y: 55, ease: Sine.easeInOut}, ".3")
     tl.to("#blue-shape", 1, {rotation: -140, scaleX: 1, scaleY: 1, x: -4, y: 117, ease: Sine.easeInOut}, ".3")
-
     tl.to("#mask-shape", .7, {rotation: 0, scaleX: 4.2, scaleY: 4.2, x: -1650, y: -1315, ease: Back.easeIn}, "1.1")
-    tl.to("#blue-shape", .7, {rotation: -230, scaleX: 0.6, scaleY: 0.6, x: -49, y: 160, ease: Sine.easeOut}, "1.45")
-
+    tl.to("#blue-shape", .7, {rotation: -230, scaleX: 0.6, scaleY: 0.6, x: -49, y: 220, ease: Sine.easeOut}, "1.45")
     tl.to("#red-shape", .7, {rotation: 0, scaleX: 4, scaleY: 4, opacity: 0, x: 575, y: -175, ease: Sine.easeInOut}, "1.45")
-    tl.from("#red-shape-base", 1, {autoAlpha: 0, xPercent: -50, ease: Sine.easeOut}, "1.4")
-    tl.from("#f1-copy", 1.5, {alpha: 0, ease: Sine.easeOut}, "1.7")
+  //  tl.from("#red-shape-base", 1, {autoAlpha: 0, xPercent: -50, ease: Sine.easeOut}, "1.4")
+    tl.from("#f1-copy", 1.5, {alpha: 0, ease: Sine.easeOut}, "1.1")
     tl.from(".bg-image", 2, {alpha: 0, ease: Sine.easeOut}, "0")
 
     //FRAME 02
     tl.to("#f1-copy", .5, {alpha: 0, ease: Sine.easeIn}, "4.7")
+    tl.to("#red-shape-base", 1.5, {y: -40,x:60, ease: Sine.easeOut}, "5.2")
     tl.from(".terms-wrapper", 1.5, {autoAlpha: 0, ease: Sine.easeOut}, "5.2")
-
-
-    tl.to("#blue-shape", .7, {x: 30, y: 390, ease: Sine.easeIn}, "9")
 //    tl.to("#red-shape", .7, {x: 25, y: 375, ease: Sine.easeIn}, "4.5")
-
-
     tl.to(".terms-wrapper", .5, {alpha: 0, ease: Sine.easeOut}, "8.5")
+    tl.to(".ff-logo-mono", .5, {alpha: 0, ease: Sine.easeOut}, "8.5")
     //tl.from("#f3-1-copy", 1, {autoAlpha: 0, ease: Sine.easeOut}, "9");
-    tl.to("#red-shape-base", 1.3, {scale:2, x: 300, ease: Sine.easeOut}, "9")
-    tl.to(".bg-image-01", 2, {alpha: 0, ease: Sine.easeOut}, "10")
-
-    tl.from(".logos", 2, {alpha: 0, ease: Sine.easeOut}, "9.2")
 
     //FRAME 03
-    //tl.to("#white-shape", 2, {autoAlpha:.6, x: 115, y: 700, rotation:-30, ease: Sine.easeInOut}, ".1")
-
-    tl.from("#f3-copy", 1, {alpha: 0, ease: Sine.easeOut}, "9")
-
-    tl.from(".terms-wrapper", 1, {alpha: 0, ease: Sine.easeOut}, "9")
+    tl.to(["#impact-copy01", "#f2-copy"], 1, {alpha: 1, ease: Sine.easeOut}, "9")
+    tl.to("#red-shape-base", 2, {scale:.4, y: -220, ease: Sine.easeOut}, "9")
+    tl.to(".bg-image-01", 2, {alpha: 0, ease: Sine.easeOut}, "9")
+    tl.to("#card-wrapper", 2, {x:0, ease: Sine.easeOut}, "9")
+    tl.to("#white-shape", 2, {x:0, alpha:1,ease: Sine.easeOut}, "9")
+    tl.to("#white-shape-2", 2, {x:0, rotation:0,ease: Sine.easeOut}, "9")
+    tl.to(".logos", 2, {x:0, ease: Sine.easeOut}, "9")
+    tl.to(".card", 2, {rotationY:180, ease:Back.easeOut}, "12")
+    tl.to("#white-shape-2", 2, {scale:.9, rotation:-14,x:9,y:-28,scaleX:1.1,scaleY:0.8,ease: Sine.easeOut}, "12")
+    tl.to(["#impact-copy01", "#f2-copy"], 1, {alpha: 0, ease: Sine.easeOut}, "12")
+    tl.to("#f3-copy", 1, {alpha: 1, ease: Sine.easeOut}, "12")
+    // tl.from(".terms-wrapper", 1, {alpha: 0, ease: Sine.easeOut}, "9")
     tl.from(".button", 1.7, {alpha: 0, scaleX: .5, scaleY: .5, ease: Expo.easeOut}, "9")
 
     // TESTING - goto specific time of animation
-    //tl.seek(8);
-    tl.pause(14)
+  //  tl.seek(13);
+  //tl.pause(3.1)
 }
 
 
